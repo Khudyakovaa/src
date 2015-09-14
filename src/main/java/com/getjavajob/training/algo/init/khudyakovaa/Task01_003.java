7914653dc9d5bbbc9ba0989a217602702f808809
khudyakovaa <nas-tya92@mail.ru>
2015-09-14 20:18:32 +0300

    Signed-off-by: khudyakovaa <nas-tya92@mail.ru>

---
+++ src/main/java/com/getjavajob/training/algo/init/khudyakovaa/Task01_003.java
@@ -0,0 +1,14 @@
+package com.getjavajob.training.algo.init.khudyakovaa;
+
+import java.util.Scanner;
+
+class Task01_003 {
+    public static void main(String args[]){
+        Scanner uinput = new Scanner(System.in);
+        int num;
+        System.out.print("Enter Number: ");
+        num = uinput.nextInt();
+        System.out.print("You've Entered: "+ num);
+        uinput.close();
+    }
+}
